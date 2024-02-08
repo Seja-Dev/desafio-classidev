276481874363f01d581540fe63bfbb98a9073c0b
GustavoCoelho295 <iago.pinheirodeoliveira@gmail.com>
2024-02-08 16:32:17 -0300

    create your ad

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -3,14 +3,28 @@ import styled from "styled-components"
 import Navbar from "@/components/navbar/navbar"
 
 const StyledDiv = styled.div`
-    background: linear-gradient(to bottom, #424242 0%, #000000 100%);
+    background: #0e0e0e;
     height: 100vh;
+`
+
+const StyledCreate = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 300px;
+    color: white;
+    font-size: 20px;
+    letter-spacing: 2px;
 `
 
 export default function Create() {
     return(
         <StyledDiv>
             <Navbar/>
+            <StyledCreate>
+                <h1>Crie seu anúncio</h1>
+            </StyledCreate>
         </StyledDiv>
     )
 }
