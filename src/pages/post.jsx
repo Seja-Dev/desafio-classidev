b03e137c5ffa9ad130ad96b58a0f59edd58cfc77
GustavoCoelho295 <iago.pinheirodeoliveira@gmail.com>
2024-02-08 23:31:55 -0300

    added components to products

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -9,12 +9,45 @@ const StyledDiv = styled.div`
     background: #0e0e0e;
 `
 
-export default function Post() {
+const ProductContainer = styled.div`
+    display: flex;
+    align-items: end;
+    gap: 20px;
+    margin-top: 50px;
+`
+
+const Product = styled.h1`
+    color: white;
+    font-weight: bolder;
+    font-size: 46px;
+`
+
+const DeleteOrEdit = styled.div`
+    display: flex;
+    gap: 7px;
+    color: ${props => props.deleted ? 'red' : 'white'};
+    text-decoration: underline;
+    padding-bottom: 7px;
+`
+
+export default function Post({ deleted }) {
     return(
         <StyledDiv>
             <Navbar/>
             <Container>
                 <Back/>
+                <ProductContainer>
+                    <Product>Palio 2020</Product>
+                    <DeleteOrEdit>
+                        <img src="edit.png" width="15px" height="15px"/>
+                        <p>Editar</p>
+                    </DeleteOrEdit>
+
+                    <DeleteOrEdit deleted>
+                        <img src="delete.png" width="15px" height="15px"/>
+                        <p>Deletar</p>
+                    </DeleteOrEdit>
+                </ProductContainer>
             </Container>
         </StyledDiv>
     )
