b40a704222b72274a270e33a23fd817933b38637
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2024-02-12 15:44:43 -0300

    Instalado dependencias

---
+++ src/pages/_app.jsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle,ThemeProvider } from 'styled-components';
+import theme from '../theme'
+
+const GlobalStyles = createGlobalStyle`
+* { 
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+html{
+ font-size: 62.5%;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+;
+`
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+        <Component {...pageProps}  />
+        <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
