5c76f39ca7788c396862a91bf9c9fce3864bafdf
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2024-02-12 15:47:18 -0300

    Criado layout  createAnnouncement page

---
+++ src/pages/createAnnouncement.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+import NavBar from "../src/components/navbar/NavBar";
+import Footer from "../src/components/footer/Footer";
+import CreateCard from "../src/components/card/CreateCard";
+
+const FooterAlt = styled(Footer)`
+  margin-top: 0px;
+`
+export default function CreateAnnouncementPage(){
+  return(
+    <>
+      <NavBar type2 />
+      <CreateCard />
+      <FooterAlt />
+    </>
+   
+  )
+}
