3242b1ca24d091d08a6c4ae01d92a92764884213
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-02-14 09:22:13 -0300

    updated delete style

--- src/components/confirmDelete/confirmdelete.jsx
+++ src/components/confirmDelete/confirmdelete.jsx
@@ -1,5 +1,20 @@
 import styled from "styled-components"
 
+const StyledDiv = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+    gap: 10px;
+`
+
 const StyledDelete = styled.div`
     background-color: #D9D9D9;
     border-radius: 10px;
@@ -26,12 +41,14 @@ const Buttons = styled.button`
 
 export default function ConfirmDelete({ onClick, cancelOnclick }) {
     return(
-        <StyledDelete>
-            <p>Tem certeza que deseja excluir?</p>
-            <ButtonsContainer>
-                <Buttons onClick={cancelOnclick}>cancelar</Buttons>
-                <Buttons delete onClick={onClick}>excluir</Buttons>
-            </ButtonsContainer>
-        </StyledDelete>
+        <StyledDiv>
+            <StyledDelete>
+                <p>Tem certeza que deseja excluir?</p>
+                <ButtonsContainer>
+                    <Buttons onClick={cancelOnclick}>cancelar</Buttons>
+                    <Buttons delete onClick={onClick}>excluir</Buttons>
+                </ButtonsContainer>
+            </StyledDelete>
+        </StyledDiv>
     )
 }
